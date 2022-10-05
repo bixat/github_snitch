b1f54184b9b2f5c1cc85e96fda888439e8d9be3f
Mohammed <m97.chahboun@gmail.com>
2022-10-05 09:29:58 +0100

    Save issues when not connected on prefs & send it later

--- lib/src/report_issue.dart
+++ lib/src/report_issue.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:github_report_issues/src/prefs.dart';
@@ -7,6 +8,7 @@ import 'package:github_report_issues/src/prefs.dart';
 import 'gh_requests.dart';
 import 'gh_response.dart';
 
+//TODO:Remove this token
 //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
 class GhReporter {
   GhReporter({required this.token, required this.owner, required this.repo}) {
@@ -17,58 +19,85 @@ class GhReporter {
   final String repo;
   late GhRequests ghRequest;
 
-  void report(
+  Future<bool> report(
       {required String title,
       required String body,
       List<String>? labels,
       List<String>? assignees,
       int? milestone}) async {
-    String issueEndpoint = "$owner/$repo/issues";
-    bool notCreated = await issueNotCreated(title, issueEndpoint);
-    if (notCreated) {
-      Map<String, dynamic> issueBody = {
-        "owner": owner,
-        "repo": repo,
-        "title": title,
-        "body": body,
-      };
-      if (assignees != null) {
-        issueBody["assignees"] = assignees;
-      }
-      if (labels != null) {
-        issueBody["labels"] = labels;
-      }
-
-      if (milestone != null) {
-        issueBody["milestone"] = milestone;
-      }
-
-      String issueBodyToString = json.encode(issueBody);
-
-      GhResponse response =
-          await ghRequest.request("POST", issueEndpoint, issueBodyToString);
-      if (response.statusCode == 201) {
-        log("✅ Issue reported");
-      } else {
-        log("❌ Echec to report Issue");
-        log(response.response.toString());
-      }
-    } else {
-      log("Issue Already Created");
-    }
-  }
-
-  Future<void> overrideExceptions() async {
-    try {
+    bool connected = await isConnected;
+    if (connected) {
       await createLabel("GhReporter-external",
           "Errors not caught by Flutter Framework", "f0c2dd");
       await createLabel("GhReporter-internal",
           "Errors caught by Flutter Framework", "6a4561");
-    } catch (e, t) {
-      log(e.toString());
-      log(t.toString());
-    }
+      String issueEndpoint = "$owner/$repo/issues";
+      bool notCreated = await issueNotCreated(title, issueEndpoint);
+      if (notCreated) {
+        Map<String, dynamic> issueBody = {
+          "owner": owner,
+          "repo": repo,
+          "title": title,
+          "body": body,
+        };
+        if (assignees != null) {
+          issueBody["assignees"] = assignees;
+        }
+        if (labels != null) {
+          issueBody["labels"] = labels;
+        }
 
+        if (milestone != null) {
+          issueBody["milestone"] = milestone;
+        }
+
+        String issueBodyToString = json.encode(issueBody);
+
+        GhResponse response =
+            await ghRequest.request("POST", issueEndpoint, issueBodyToString);
+        if (response.statusCode == 201) {
+          log("✅ Issue reported");
+          return true;
+        } else {
+          log("❌ Echec to report Issue");
+          log(response.response.toString());
+          return false;
+        }
+      } else {
+        log("Issue Already Created");
+        return true;
+      }
+    } else {
+      Map issue = {
+        "title": title,
+        "body": body,
+      };
+      String issueToString = json.encode(issue);
+      Prefs.set("github_report_issue${title.hashCode}", issueToString);
+      return false;
+    }
+  }
+
+  reportSavedIssues() async {
+    List prefsKeys = (await Prefs.getKeys()).toList();
+    List olderIssues =
+        prefsKeys.where((e) => e.contains("github_report_issue")).toList();
+    print(olderIssues);
+    if (olderIssues.isNotEmpty) {
+      for (var e in olderIssues) {
+        String? issueFromPref = await Prefs.get(e);
+        var issueToMap = json.decode(issueFromPref!);
+        bool reported =
+            await report(title: issueToMap["title"], body: issueToMap["body"]);
+        if (reported) {
+          Prefs.remove(e);
+        }
+      }
+    }
+  }
+
+  Future<void> initialze() async {
+    reportSavedIssues();
     FlutterError.onError = (details) {
       FlutterError.presentError(details);
       bool issueNotFromPackage =
@@ -95,7 +124,7 @@ class GhReporter {
   }
 
   createLabel(String label, String description, String color) async {
-    bool labelNotCreated = !(await Prefs.getLabel(label));
+    bool labelNotCreated = !(await Prefs.labelExist(label));
     if (labelNotCreated) {
       String labelEndpoint = "$owner/$repo/labels";
       Map labelBody = {
@@ -117,13 +146,9 @@ class GhReporter {
   }
 
   Future<bool> issueNotCreated(String title, String endpoint) async {
-    Map<dynamic, dynamic> filterBody = {
-      "state": "all",
-      "labels": ["GhReporter-external", "GhReporter-external"]
-    };
-    String encodeBody = json.encode(filterBody);
+    String params = "?state=all&labels=GhReporter-external";
     GhResponse ghResponse =
-        await ghRequest.request("GET", "$endpoint?title=$title", encodeBody);
+        await ghRequest.request("GET", endpoint + params, "");
     if (ghResponse.statusCode == 200) {
       bool notExist = true;
       for (var e in (ghResponse.response as List)) {
@@ -134,5 +159,16 @@ class GhReporter {
       return notExist;
     }
     return false;
+  }
+
+  Future get isConnected async {
+    try {
+      final result = await InternetAddress.lookup('example.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        return true;
+      }
+    } on SocketException catch (_) {
+      return false;
+    }
   }
 }
