b0ec3e13dabb2f8abd8cd7b520a0a71dea0318ac
Mohammed <m97.chahboun@gmail.com>
2022-10-05 09:30:26 +0100

    Enhance example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:github_report_issues/github_report_issues.dart';
 
 void main() {
-  GhReporter githubIssue = GhReporter(
-      owner: 'M97chahboun',
-      token: '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',
-      repo: 'flutter_ci_cd');
-
-  githubIssue.overrideExceptions();
+  if (kReleaseMode) {
+    WidgetsFlutterBinding.ensureInitialized();
+    GhReporter ghReporter = GhReporter(
+        owner: 'M97chahboun',
+        token: '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',
+        repo: 'gitflow');
+    ghReporter.initialze();
+  }
   runApp(const MyApp());
 }
 
@@ -56,6 +59,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   void _incrementCounter() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return const Screen([]);
+    }));
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
@@ -66,9 +73,10 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  late String h;
+  int h = 55;
   @override
   Widget build(BuildContext context) {
+    print("hello");
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -81,42 +89,49 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(h),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Row(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Invoke "debug painting" (press "p" in the console, choose the
+        // "Toggle Debug Paint" action from the Flutter Inspector in Android
+        // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+        // to see the wireframe for each widget.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+            'You have pushed the button this many times:',
+          ),
+          //Text(h),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Screen extends StatelessWidget {
+  const Screen(this.wrong, {super.key});
+  final wrong;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Hello" + wrong),
     );
   }
 }
