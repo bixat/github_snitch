e89d0a5e2c379ceb9f4ede7761cf69243923a5d6
Mohammed <m97.chahboun@gmail.com>
2022-10-09 19:54:11 +0100

    Removed unused exports

--- lib/github_reporter.dart
+++ lib/github_reporter.dart
@@ -1,7 +1,2 @@
 library github_reporter;
-export 'src/report_issue.dart';
-export 'src/utils/constants.dart';
-export 'src/utils/extensions.dart';
 export 'src/utils/gh_reporeter_delegate.dart';
-export 'src/utils/constants.dart';
-export 'src/utils/constants.dart';
